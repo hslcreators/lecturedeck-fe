31f687de1470934ca3df983c355f43f04d13bb79
RalphFred <raphaelgbaorun@gmail.com>
2023-11-12 18:06:26 +0100

    Build: Installesd the necessary dependencies

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Construction from './assets/Construction.png'
+
+export default function App() {
+  return (
+    <div>
+      <img src={Construction} className='w-full h-screen'/>
+    </div>
+  );
+}
